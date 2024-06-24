a1cc9a7984e1a4683588146005a43373b9e28776
samkamau254 <samkamau254@outlook.com>
2024-06-24 09:59:47 -0400

    overwritting avoid

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,9 +4,9 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
     
-	  const task = true;
+	  const task = false
     
-	  const task2 = false;
+	  const task2 = true;
   }
 
   return [task, task2];
