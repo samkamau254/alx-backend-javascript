0756355dc5c79bd1d317d6596dffffb7f705c3f9
samkamau254 <samkamau254@outlook.com>
2024-06-24 10:18:35 -0400

    overwritting avoid

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-	  // eslint-disable-line no-unused-vars
-	   const task = false;
-	  // eslint-disable-line no-unused-vars
-	   const task2 = true;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
